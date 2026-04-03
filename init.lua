e5c464016a44507a6535673a4c7ee3615360df00
Jackson Crim <jacksoncrim@gmail.com>
2026-04-02 22:27:55 -0400

    .md preview plugin added to init.lua

--- init.lua
+++ init.lua
@@ -285,6 +285,17 @@ require('lazy').setup({
   { import = 'kickstart.plugins.gitsigns' },
   { import = 'kickstart.plugins.indent_line' },
 
+  -- .md preview plugin
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+
   -- Alternatively, use `config = function() ... end` for full control over the configuration.
   -- If you prefer to call `setup` explicitly, use:
   --    {
