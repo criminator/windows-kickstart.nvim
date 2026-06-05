6e59585f05b526247ab1a6fec9d4bfabd2f32039
Jackson Crim <jacksoncrim@ymail.com>
2026-06-04 23:10:24 -0400

    updates, there are some bugs, probably will redo with new kickstart soon

--- init.lua
+++ init.lua
@@ -661,7 +661,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        -- ts_ls = {}, -- for js
         emmet_language_server = {
           filetypes = {
             'html',
